86601c0b1de6f1cc89432285313076826b9e58e0
seb.nor.san <sejeseb99@gmail.com>
2025-07-29 18:04:31 +0200

    Changed remote npc

--- Assets/NPC_Interactable_Remote.cs
+++ Assets/NPC_Interactable_Remote.cs
@@ -2,24 +2,48 @@ using UnityEngine;
 
 public class NPC_Interactable_Remote : MonoBehaviour, IInteractable
 {
+	public static NPC_Interactable_Remote instance;
     [SerializeField] private NPC_Interactable npcOwner;
+	private Animator anim;
 
-	public bool canInteract { get ; set ; }
+	public bool canInteract { get; set; } = true;
 
-	private void OnEnable()
+	private void Awake()
 	{
-		npcOwner.OnTalkEnded += TalkEnded;	
+		if (instance != null)
+			Destroy(gameObject);
+		else
+			instance = this;
 	}
 	private void Start()
 	{
-		canInteract = npcOwner.canInteract;
+		anim = GetComponent<Animator>();
+	}
+	private void OnEnable()
+	{
+		npcOwner.OnTalkEnded += TalkEnded;
 	}
 	public void Interact()
 	{
+		StartCoroutine(PickUp());
+	}
+	System.Collections.IEnumerator PickUp()
+	{
+		canInteract = false;
+
+		anim.SetTrigger("Answer");
+		yield return new WaitForSeconds(GetAnimationLength() + .4f);
 		npcOwner.Interact();
 	}
 	private void TalkEnded()
 	{
-
+		anim.SetTrigger("HangUp");
+		Invoke(nameof(ResetInteract), GetAnimationLength() + .4f);
+	}
+	private float GetAnimationLength() => anim.GetCurrentAnimatorStateInfo(0).length;
+	private void ResetInteract() => canInteract = true;
+	public void CallUp()
+	{
+		anim.SetTrigger("Call");
 	}
 }
